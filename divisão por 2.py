b69f86048b3bcd8f240e29e7f5e3bebee9097139
Jhonatan Paz <jhonatanpaaz@gmail.com>
2023-10-12 11:03:38 -0300

    Update divisão por 2.py

--- divisão por 2.py
+++ divisão por 2.py
@@ -11,7 +11,7 @@ idade_usuario = input('digite sua idade: ')
 print(f'prazer em te conhecer {nome_usuario}!')
 print('vamos verificar se um numero é par?!')
 
-digite_um_numero = int(input(f'Agora digite um numero, {nome_usuario} :'))
+digite_um_numero = int(input(f'Agora digite um numero, {nome_usuario} :')) #trabalhar com try/except
 
 if  digite_um_numero % 2 == 0:
     print('O numero que você digitou é par (ou seja, a divisão desse número por 2 tem resto igual a 0)')
